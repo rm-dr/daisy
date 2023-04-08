1e04b15b46614e661ce8b2accd4c7bdab3b0a609
Mark <mark@betalupi.com>
2023-04-07 20:55:23 -0700

    Added comments

--- src/quantity/mod.rs
+++ src/quantity/mod.rs
@@ -7,6 +7,23 @@ use std::ops::{
 };
 
 
+/*
+Quantity:
+	Represents a value with a unit attached to it.
+Units have yet to be implemented.
+
+f64q: a quantity based on plain f64s
+floatq: a quantity using rug bigfloat
+rationalq: a quantity using rug rationals
+
+All of the above are ONLY used for values.
+There is only one kind of unit type.
+
+
+The cfg_if blocks here are a temporary hack to allow for
+cross-compilation to other systems. RUG does not work on all systems.
+*/
+
 pub mod quantity;
 
 cfg_if::cfg_if! {
